171f7a4c1373f5e216f5fbe089311ca4a100bc8d
James Hiew <james@hiew.net>
2017-12-06 21:34:44 +0000

    Accept one argument on the command line; just print it for now
    This argument will be the name of the file to compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    for (i, arg) in env::args().enumerate() {
+        if i == 0 {
+            continue
+        }
+        if i == 1 {
+            println!("{}", arg);
+        }
+    }
 }
