257de19b3fba63186f3ed967a30ea947fcbb5044
James Hiew <james@hiew.net>
2017-12-06 21:48:09 +0000

    Read in the contents of the passed filename and print to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-use std::env;
+use std::{env, fs, io, process};
+use std::io::Read;
 
 fn main() {
     for (i, arg) in env::args().enumerate() {
         if i == 0 {
             continue
         }
-        if i == 1 {
-            println!("{}", arg);
+        if i == 1 {  // filename
+            let file;
+            match fs::File::open(arg) {
+                Ok(f) => file = f,
+                Err(error) => {
+                    eprintln!("{}", error);
+                    process::exit(1);
+                },
+            };
+            let mut reader = io::BufReader::new(file);
+            let mut contents = String::new();
+            let bytes_read;
+            match reader.read_to_string(&mut contents) {
+                Ok(b) => bytes_read = b,
+                Err(error) => {
+                    eprintln!("{}", error);
+                    process::exit(1);
+                }
+            };
+            println!("{}", contents);
         }
     }
 }
